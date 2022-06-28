b2415b62202ccfe3af91922cf6cccd6e9b5e8303
mishrasub <subhash06582@gmail.com>
2022-06-28 12:35:18 +0530

    initial commit for OAuth2 Client using GitHub

---
+++ src/main/java/com/poc/oauth/SpringSecOauthGitHubApplication.java
@@ -0,0 +1,13 @@
+package com.poc.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecOauthGitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecOauthGitHubApplication.class, args);
+	}
+
+}
